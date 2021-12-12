93ece3078761f106dcff38b178f352c8b0d4749e
anhnhan1702 <68462921+anhnhan1702@users.noreply.github.com>
2021-12-12 13:38:51 +0700

    sửa view chat , chat list

--- client/src/components/chatlist.vue
+++ client/src/components/chatlist.vue
@@ -1,209 +1,253 @@
 <template>
-  <div class="chatlist">
+<div class="chatlist">
     <div class="chatlist-content">
-      <div class="header">
-        <div  v-b-toggle.my-sidebar class="icon"
-          ><i class="fas fa-bars"></i
-        ></div>
-        <b-sidebar id="my-sidebar" title="Sidebar" shadow>
-          <div class="px-3 py-2">
-            Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-            dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-            ac consectetur ac, vestibulum at eros.
-          </div>
-        </b-sidebar>
-        <input class="input-search" type="search" placeholder="Tìm kiếm ..." />
-      </div>
-      <div class="chat-list">
-        <div class="users" v-for="user in users" :key="user.id">
-          <img src="../assets/images/cat.jpg" alt="" />
-          <div class="content">
-            <div class="content-top">
-              <div class="name">{{ user.name }}</div>
-              <div class="time">{{ user.time }}</div>
-            </div>
-            <div class="content-bot">
-              <div class="message">
-                <div>{{ user.latestUser }}:</div>
-                <div>{{ user.latestMessage }}</div>
-              </div>
-              <div class="unread-message">
-                <div class="number">{{ user.unreadMessage }}</div>
-              </div>
-            </div>
-          </div>
+        <div class="header">
+            <div v-b-toggle.my-sidebar class="icon"><i class="fas fa-bars"></i></div>
+            <b-sidebar id="my-sidebar" title="Sidebar" shadow>
+                <div class="px-3 py-2">
+                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+                    dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
+                    ac consectetur ac, vestibulum at eros.
+                </div>
+            </b-sidebar>
+            <input class="input-search" type="search" placeholder="Tìm kiếm ..." />
         </div>
-      </div>
+        <div class="chat-list">
+            <div v-for="user in users" :key="user.id">
+                <div class="users" v-if="user.position == 1" style="background: #3390ec;  border-radius: 10px;">
+                    <img src="../assets/images/cat.jpg" alt="" />
+                    <div class="content">
+                        <div class="content-top">
+                            <div class="name" style="color: white;">{{ user.name }}</div>
+                            <div class="time" style="color: white;">{{ user.time }}</div>
+                        </div>
+                        <div class="content-bot">
+                            <div class="message" >
+                                <div style="color: white;">{{ user.latestUser }}:</div>
+                                <div style="color: white;">{{ user.latestMessage }}</div>
+                            </div>
+                            <div class="unread-message" style=" background-color: white;" >
+                                <div class="number"  style=" color:  #3390ec; ">{{ user.unreadMessage }}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="users" v-else>
+                    <img src="../assets/images/cat.jpg" alt="" />
+                    <div class="content">
+                        <div class="content-top">
+                            <div class="name">{{ user.name }}</div>
+                            <div class="time">{{ user.time }}</div>
+                        </div>
+                        <div class="content-bot">
+                            <div class="message">
+                                <div>{{ user.latestUser }}:</div>
+                                <div>{{ user.latestMessage }}</div>
+                            </div>
+                            <div class="unread-message">
+                                <div class="number">{{ user.unreadMessage }}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
+</div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      users: [
-        {
-          image: "../assets/images/cat.jpg",
-          name: "Nguyen",
-          time: "2:39 PM",
-          latestUser: "Manh",
-          latestMessage: "AhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihi",
-          unreadMessage: 150,
-        },
-        {
-          image: "../assets/images/cat.jpg",
-          name: "Nguyen",
-          time: "2:39 PM",
-          latestUser: "Manh",
-          latestMessage: "AhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihi",
-          unreadMessage: 150,
-        },
-        {
-          image: "../assets/images/cat.jpg",
-          name: "Nguyen",
-          time: "2:39 PM",
-          latestUser: "Manh",
-          latestMessage: "AhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihi",
-          unreadMessage: 150,
-        },
-        {
-          image: "../assets/images/cat.jpg",
-          name: "Nguyen",
-          time: "2:39 PM",
-          latestUser: "Manh",
-          latestMessage: "AhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihi",
-          unreadMessage: 150,
-        },
-        {
-          image: "../assets/images/cat.jpg",
-          name: "Nguyen",
-          time: "2:39 PM",
-          latestUser: "Manh",
-          latestMessage: "AhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihi",
-          unreadMessage: 150,
-        },
-        {
-          image: "../assets/images/cat.jpg",
-          name: "Nguyen",
-          time: "2:39 PM",
-          latestUser: "Manh",
-          latestMessage: "AhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihi",
-          unreadMessage: 150,
-        },
-        {
-          image: "../assets/images/cat.jpg",
-          name: "Nguyen",
-          time: "2:39 PM",
-          latestUser: "Manh",
-          latestMessage: "AhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihi",
-          unreadMessage: 150,
-        },
-      ],
-    };
-  },
+    data() {
+        return {
+            users: [{
+                    image: "../assets/images/cat.jpg",
+                    name: "Nguyen",
+                    time: "2:39 PM",
+                    latestUser: "Manh",
+                    latestMessage: "AhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihi",
+                    unreadMessage: 150,
+                    position: 1,
+                },
+                {
+                    image: "../assets/images/cat.jpg",
+                    name: "Nguyen",
+                    time: "2:39 PM",
+                    latestUser: "Manh",
+                    latestMessage: "AhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihi",
+                    unreadMessage: 150,
+                          position: 0,   
+                },
+                {
+                    image: "../assets/images/cat.jpg",
+                    name: "Nguyen",
+                    time: "2:39 PM",
+                    latestUser: "Manh",
+                    latestMessage: "AhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihi",
+                    unreadMessage: 150,
+                              position: 0,   
+                },
+                {
+                    image: "../assets/images/cat.jpg",
+                    name: "Nguyen",
+                    time: "2:39 PM",
+                    latestUser: "Manh",
+                    latestMessage: "AhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihi",
+                    unreadMessage: 150,
+                      position: 0,   
+                },
+                {
+                    image: "../assets/images/cat.jpg",
+                    name: "Nguyen",
+                    time: "2:39 PM",
+                    latestUser: "Manh",
+                    latestMessage: "AhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihi",
+                    unreadMessage: 150,
+                            position: 0,   
+                },
+                {
+                    image: "../assets/images/cat.jpg",
+                    name: "Nguyen",
+                    time: "2:39 PM",
+                    latestUser: "Manh",
+                    latestMessage: "AhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihi",
+                    unreadMessage: 150,
+                            position: 0,   
+                },
+                {
+                    image: "../assets/images/cat.jpg",
+                    name: "Nguyen",
+                    time: "2:39 PM",
+                    latestUser: "Manh",
+                    latestMessage: "AhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihi",
+                    unreadMessage: 150,
+                            position: 0,   
+                },
+            ],
+        };
+    },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
 .chatlist {
-  height: 100%;
-  width: 100%;
-  overflow-y: hidden;
+    height: 100%;
+    width: 100%;
+    overflow-y: hidden;
+   
 }
+
 .chatlist-content {
-  padding: 10px 10px 10px 10px;
+    padding: 10px 10px 10px 10px;
 }
+
 .header {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 }
+
 .icon {
-  width: 35px;
-  height: 35px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-right: 10px;
+    width: 35px;
+    height: 35px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 10px;
 }
+
 .icon:hover {
-  background: gray;
-  border-radius: 50%;
+    background: gray;
+    border-radius: 50%;
 }
 
 .input-search {
-  width: 90%;
-  height: 45px;
-  overflow: hidden;
-  border-radius: 22px;
+    padding-left: 20px;
+    width: 90%;
+    height: 45px;
+    overflow: hidden;
+    border-radius: 22px;
 }
 
 .chat-list {
-  width: 100%;
-  margin-top: 5px;
+    width: 100%;
+    margin-top: 5px;
 }
+
 .users {
-  display: flex;
-  align-items: center;
-  width: 100%;
-  padding: 5px 5px 5px 5px;
+    display: flex;
+    align-items: center;
+    width: 100%;
+    padding: 5px 5px 5px 5px;
 }
+
 .users:hover {
-  background: #dfe1e5;
-  border-radius: 10px;
+    background: #dfe1e5;
+    border-radius: 10px;
 }
+
 .users img {
-  width: 15%;
-  border-radius: 50%;
+    width: 15%;
+    border-radius: 50%;
 }
+
 .content {
-  width: 85%;
-  margin-left: 10px;
+    width: 85%;
+    padding-left: 10px;
 }
+
 .content-top {
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  margin-bottom: 0px;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    margin-bottom: 0px;
 }
+
 .content-bot {
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  align-items: center;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    align-items: center;
 }
+
 .name {
-  font-weight: bold;
-  font-size: 18px;
+    font-weight: bold;
+    font-size: 18px;
 }
+
 .time {
-  color: rgba(128, 128, 128, 0.877);
-  font-size: 14px;
+    color: rgba(128, 128, 128, 0.877);
+    font-size: 14px;
 }
+
 .message {
-  display: flex;
-  font-size: 16px;
+    display: flex;
+    font-size: 16px;
+    width: 85%;
 }
+
 .message div:last-child {
-  color: rgba(128, 128, 128, 0.877);
-  white-space: nowrap;
-  width: 200px;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  padding-left: 2px;
+    color: rgba(128, 128, 128, 0.877);
+    white-space: nowrap;
+    overflow: hidden;
+    width: 100%;
+    text-overflow: ellipsis;
+    padding: 0 5px;
 }
+
 .unread-message {
-  background: green;
-  border-radius: 50%;
-  height: 30px;
-  width: 30px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+    background: #00c73e;
+    border-radius: 50%;
+    height: 30px;
+    width: 15%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
+
 .number {
-  color: white;
+    color: white;
 }
 </style>
--- client/src/components/chatview.vue
+++ client/src/components/chatview.vue
@@ -1,117 +1,172 @@
 <template>
-  <div class="chatview">
-    <div class="background">
-      <img src="../assets/images/green.png" alt="" />
-    </div>
+<div class="chatview">
+
     <div class="header">
-      <img src="../assets/images/cat.jpg" alt="" />
-      <div class="box-infor" v-for="user in users" :key="user.id">
-        <div class="information">
-          <div class="name">{{ user.name }}</div>
-          <div class="member">{{ user.member }} members</div>
+        <img src="../assets/images/cat.jpg" alt="" />
+        <div class="box-infor" v-for="user in users" :key="user.id">
+            <div class="information">
+                <div class="name">{{ user.name }}</div>
+                <div class="member">{{ user.member }} members</div>
+            </div>
+            <div v-b-toggle.my-collapse><i class="fas fa-ellipsis-v"></i></div>
+            <b-collapse id="my-collapse">
+                <b-card title="Collapsible card"> Hello world! </b-card>
+            </b-collapse>
         </div>
-        <div v-b-toggle.my-collapse><i class="fas fa-ellipsis-v"></i></div>
-        <b-collapse id="my-collapse">
-          <b-card title="Collapsible card"> Hello world! </b-card>
-        </b-collapse>
-      </div>
     </div>
     <div class="boxchat">
-      <input type="text" class="input-text" placeholder="Nhập tin nhắn" />
-      <div class="chat">AhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihi</div>
-      <div class="your-text">AhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihi</div>
+        <div class="boxchat-content">
+            <div class="chat">AhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihi</div>
+            <div class="your-text">AhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihi</div>
+
+            <div class="message-footer ">
+                <div class="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" style="color: #7d8185;" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-smile">
+                        <circle cx="12" cy="12" r="10"></circle>
+                        <path d="M8 14s1.5 2 4 2 4-2 4-2"></path>
+                        <line x1="9" y1="9" x2="9.01" y2="9"></line>
+                        <line x1="15" y1="9" x2="15.01" y2="9"></line>
+                    </svg>
+                </div>
+                <div class="message-input">
+                    <input type="text" class="input-content" placeholder="Nhập tin nhắn" />
+                </div>
+                <div class="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" style="color: #7d8185;" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-paperclip">
+                        <path d="M21.44 11.05l-9.19 9.19a6 6 0 0 1-8.49-8.49l9.19-9.19a4 4 0 0 1 5.66 5.66l-9.2 9.19a2 2 0 0 1-2.83-2.83l8.49-8.48"></path>
+                    </svg>
+                </div>
+            </div>
+        </div>
+
     </div>
-  </div>
+</div>
 </template>
+
 <script>
 export default {
-  data() {
-    return {
-      users: [
-        {
-          image: "../assets/images/cat.jpg",
-          name: "Nguyen",
-          time: "2:39 PM",
-          latestUser: "Manh",
-          latestMessage: "AhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihi",
-          unreadMessage: 150,
-          member: 10,
-        },
-      ],
-    };
-  },
+    data() {
+        return {
+            users: [{
+                image: "../assets/images/cat.jpg",
+                name: "Nguyen",
+                time: "2:39 PM",
+                latestUser: "Manh",
+                latestMessage: "AhihiAhihiAhihiAhihiAhihiAhihiAhihiAhihi",
+                unreadMessage: 150,
+                member: 10,
+            }, ],
+        };
+    },
+
 };
 </script>
+
 <style scoped>
+/* message-footer */
+.boxchat .message-footer {
+    width: 80%;
+    margin: 1% 10%;
+    height: 50px;
+    background-color: white;
+    display: flex;
+    align-items: center;
+    border-radius: 10px;
+
+}
+
+.boxchat .message-footer .icon {
+    width: 5%;
+    display: flex;
+    justify-content: space-around;
+}
+
+.boxchat .message-footer .message-input {
+    width: 90%;
+}
+
+.boxchat .message-footer .message-input .input-content {
+    width: 100%;
+    border: none;
+}
+.boxchat .message-footer .message-input .input-content:focus-visible {
+    outline: none;
+    border: none;
+}
+
+/*  */
 .chatview {
-  width: 100%;
-  height: 100%;
-  position: relative;
+    width: 100%;
+    height: 100%;
+    position: relative;
+    background-image: url("../assets/images/green.png");
+    width: 100%;
+    height: 100vh;
+    z-index: -9999999;
 }
+
 .background img {
-  width: 100%;
-  height: 100vh;
-  z-index: -9999999;
+    width: 100%;
+    height: 100vh;
+    z-index: -9999999;
 }
+
 .header {
-  display: flex;
-  width: 100%;
-  border-bottom: 1px solid black;
-  align-items: center;
-  padding: 5px 10px 5px 10px;
-  position: fixed;
-  top: 0;
-  z-index: 999999999;
-  background: #ffffff;
+    display: flex;
+    width: 100%;
+    /* border-bottom: 1px solid black; */
+    box-shadow: 0 2px 2px #726f702b;
+    align-items: center;
+    padding: 5px 10px 5px 10px;
+    position: fixed;
+    top: 0;
+    z-index: 999999999;
+    background: #ffffff;
 }
+
 .header img {
-  height: 45px;
-  width: 45px;
-  border-radius: 50%;
+    height: 45px;
+    width: 45px;
+    border-radius: 50%;
 }
+
 .box-infor {
-  padding-left: 20px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
+    padding-left: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
 }
+
 .boxchat {
-  padding: 0 100px 0 100px;
-  position: absolute;
-  top: 58px;
-  display: flex;
-  align-items: center;
-  width: 100%;
-  justify-content: center;
-  height: 90%;
+    display: flex;
+    width: 100%;
+    height: 100%;
+    align-items: end;
 }
-.input-text {
-  position: absolute;
-  bottom: 0;
-  width: 75%;
-  height: 50px;
-  overflow: hidden;
-  border-radius: 22px;
-  z-index: 99;
+
+.boxchat .boxchat-content {
+    width: 100%;
 }
+
 .chat {
-  position: absolute;
-  display: flex;
-  align-items: center;
-  background: #fff;
-  left: 15%;
-  padding: 3px;
-  border-radius: 5px;
+    /* position: absolute;
+    display: flex;
+    align-items: center;
+    background: #fff;
+    left: 15%;
+    padding: 3px;
+    border-radius: 5px; */
 }
+
 .your-text {
-  position: absolute;
-  display: flex;
-  align-items: center;
-  background: #fff;
-  right: 15%;
-  padding: 3px;
-  border-radius: 5px;
-  margin-top: 70px;
+    /* position: absolute;
+    display: flex;
+    align-items: center;
+    background: #fff;
+    right: 15%;
+    padding: 3px;
+    border-radius: 5px;
+    margin-top: 70px; */
 }
 </style>
